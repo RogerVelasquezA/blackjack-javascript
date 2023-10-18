926da89e778be1cfd50c9e7ed4e96046bf794ec8
rvelasquez <rogervelasquez66@gmail.com>
2023-10-18 01:40:31 -0500

    blackjack 3

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -2,7 +2,7 @@ let deck = [];
 let tipos = ['C','D','H','S']
 let especiales = ['A','K','Q','J']
 
-const baraja =() =>{
+const crearDeck = () =>{
     for(let i= 2; i<=10;i++){
         for(let tip of tipos){
             deck.push(i+tip)
@@ -14,15 +14,46 @@ const baraja =() =>{
         }
         
     }
+    deck= _.shuffle(deck)//_.shuffle es para ordenar manera aleatoria el arreglo
     console.log(deck)
-
-    deck= _.shuffle(deck)
-
-
-    console.log(deck)
-
     return deck;
 }
 
-baraja()
+crearDeck();
 
+//Funcion para pedir una carta
+
+const tomarCarta = () =>{
+    if(deck.length === 0){
+        throw "No hay Cartas";
+    }
+
+    let cartaTomada;
+
+    cartaTomada = deck.pop();
+
+    console.log(cartaTomada);
+    console.log(deck);
+    return cartaTomada;
+}
+
+const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length-1)
+    return (isNaN(valor)) ?
+            (valor==='A') ? 11: 10
+            :valor * 1
+    // let puntos = 0;
+    // if(isNaN(valor)){
+    //     console.log("No es numero")
+    // }else{
+    //     console.log("Si es un numero")
+    //     puntos = valor
+        
+    // }
+    // console.log(puntos*1)
+
+}
+
+let valor = valorCarta(tomarCarta())
+
+console.log({valor})
